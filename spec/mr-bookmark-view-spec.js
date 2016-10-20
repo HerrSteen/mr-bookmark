34d1e8184980b4c8fa4f99be76a390346a39a964
HerrSteen <pastor.steen@gmail.com>
2016-10-20 19:46:50 +0200

    first

---
+++ spec/mr-bookmark-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MrBookmarkView from '../lib/mr-bookmark-view';
+
+describe('MrBookmarkView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
