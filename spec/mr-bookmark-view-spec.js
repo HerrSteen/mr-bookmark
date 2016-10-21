9890c05f921588c6813d5cb4d148cfd7ea8b8c98
HerrSteen <pastor.steen@gmail.com>
2016-10-21 22:27:26 +0200

    Refactoring

--- spec/mr-bookmark-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import MrBookmarkView from '../lib/mr-bookmark-view';
-
-describe('MrBookmarkView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
