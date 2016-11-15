16a1e730934a779ccb91a77c9637f9915030d265
HerrSteen <pastor.steen@gmail.com>
2016-11-15 22:35:29 +0100

    Cleans up tests

--- spec/mr-bookmark-feature-spec.js
+++ spec/mr-bookmark-feature-spec.js
@@ -1,29 +1,19 @@
 'use babel';
 
-// import MrGit from '../lib/mr-git';
 import gitHelper from './helpers/git-helper';
 import workspaceHelper from './helpers/workspace-helper';
-// import FileListView from "../lib/file-list-view";
 
 describe('Mr Bookmark', () => {
-  let workspaceElement, container;
+  let workspaceElement, container, mainFolder;
 
   before(() => {
+    mainFolder = "folder"
     workspaceElement = atom.views.getView(atom.workspace)
   });
 
   before(() => {
     atom.workspace.getActivePaneItem = workspaceHelper.getActivePaneItem;
-    workspaceHelper.setEditorFile("folder/file1.js")
-  });
-
-  before(() => {
     atom.project.getPaths = workspaceHelper.getPaths;
-  });
-
-
-
-  it("has a fake repo", () => {
     atom.project.getRepositories = () => {
       return [gitHelper];
     };
@@ -35,10 +25,6 @@ describe('Mr Bookmark', () => {
     });
   });
 
-  it("Contains file in buffer", () => {
-
-  });
-
   it("has its own container", () => {
     container = workspaceElement.querySelector(".mr-bookmark-panel");
     expect(container).to.exist;
@@ -46,17 +32,20 @@ describe('Mr Bookmark', () => {
 
   it("Adds files", () => {
     const addButton = workspaceElement.getElementsByClassName("add-button")[0];
-    addButton.click();
+
+    ["f1.js", "f2.js", "f3.js"].forEach((arr) => {
+      workspaceHelper.setEditorFile(`${mainFolder}/${arr}`);
+      addButton.click();
+    });
   });
 
-  it("containes added files", () => {
+  it("contains added files", () => {
     let items = container.getElementsByClassName('list-item');
-    console.log("items", items);
-    expect(items.length).to.equal(1);
+    expect(items.length).to.equal(3);
   });
 
-  it("has the right color if its git modified", () => {
-    gitHelper.triggerChangeStatus("folder/file1.js", 256);
+  it("has the right color if it's git modified", () => {
+    gitHelper.triggerChangeStatus(`${mainFolder}/f1.js`, 256);
     let items = container.getElementsByClassName('git-modified');
     expect(items.length).to.equal(1);
   });
